ad8435473b4da660bdf9daa677f7254fccf0458f
penfold00 <109156188+penfold00@users.noreply.github.com>
2023-10-02 16:01:15 +0200

    03 solved

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+  chars = string.split("");
+  let rev = "";
+  for (i = 1; i <= chars.length; i++) {
+    rev += chars[chars.length - i];
+  }
+  return rev;
 };
-
 // Do not edit below this line
 module.exports = reverseString;
