94ad4c047af5cb6df506244e1d1c8b461e17e785
penfold00 <109156188+penfold00@users.noreply.github.com>
2023-10-03 10:03:30 +0200

    04 solved

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function (array, ...toRemove) {
+  for (item of toRemove) {
+    let removeIndex = array.indexOf(item);
+    if (removeIndex >= 0) {
+      array.splice(removeIndex, 1);
+    }
+  }
+  return array;
 };
 
 // Do not edit below this line
